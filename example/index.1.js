43f6e387c8682e79933745c75f4647a9e72f10fc
Gonzalo Nandez <godan2310@yahoo.es>
2017-12-30 05:22:45 +0100

    test partial template

--- example/index.1.js
+++ example/index.1.js
@@ -6,13 +6,13 @@ vueToHtml.registerPartialsDir(path.resolve(__dirname, 'partials'));
 const html = vueToHtml.render(`
 <div class="root">
     <div v-for="post in posts" class="post">
-        <partial:test :post="post" />
+        <partial:test :post="post" :author="post.author"/>
     </div>  
 </div>
 `, {
     posts: [
         { title: 'foo', author: 'john' },
-        { title: 'bar', message: 'message sent from beyond' },
+        { title: 'bar' },
     ],
     foo: 'foo is also working'
 });
